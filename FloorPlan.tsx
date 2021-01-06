6afb33b375c37389233a24397a116b5ee7f58335
Andreas Eisenreich <andreas.eisenreich@posteo.net>
2021-01-06 22:33:18 +0100

    Grundriss

---
+++ FloorPlan.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="FloorPlan" tilewidth="32" tileheight="32" tilecount="2860" columns="52">
+ <image source="../../../Downloads/Bildschirmfoto 2021-01-05 um 13.34.28.png" width="1687" height="1777"/>
+</tileset>
